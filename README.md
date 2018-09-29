ce506fc333f5c29b88319380abdc0e0f9855765b
chenxin <chenxin@funtsui.com>
2018-09-29 16:25:05 +0800

    格式化 Readme

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # 单页面博客 HeartBeat
+
 [![Author](https://img.shields.io/badge/author-chanshiyucx-blue.svg?style=flat-square)](https://chanshiyu.com)
 [![QQ](https://img.shields.io/badge/QQ-1124590931-blue.svg?style=flat-square)](http://wpa.qq.com/msgrd?v=3&uin=&site=qq&menu=yes)
 [![Email](https://img.shields.io/badge/Emali%20me-me@chanshiyu.com-green.svg?style=flat-square)]()
 
 ## 关于 HeartBeat
+
 HeartBeat 是一个使用 Preact 开发的 SPA 单页面博客应用程序，后台数据源依托于 [Github Issues](https://developer.github.com/v3/issues/)，前端框架使用 [UmiJS](https://umijs.org/)。此外，还使用开源项目 [Gitalk](https://github.com/gitalk/gitalk) 作为博客的评论系统，基本上使用了 Github 全家桶。
 
 技术栈：UmiJS + Github Issues + Gitalk
 
-演示地址：[蝉時雨 ](https://chanshiyu.com)  
+演示地址：[蝉時雨](https://chanshiyu.com)
 
 ## 食用指南
+
 ### 下载并安装
+
 ```bash
 git@github.com:chanshiyucx/HeartBeat.git
 cd HeartBeat
@@ -19,17 +23,21 @@ npm install
 ```
 
 ### 配置
+
 修改目录 `src/config.js` 的配置文件，每个配置项都有详细说明。
 
 ### 预览
+
 ```bash
 npm start
 ```
 
 ### 打包和发布
+
 ```bash
 umi build
 ```
+
 打包完毕，将 `dist` 目录下生成的静态文件发布 Github Pages 或 Coding Pages 即可。
 
 Just enjoy it ฅ●ω●ฅ
--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,49 @@
-import timeago from 'timeago.js'
-import config from './config'
+import timeago from "timeago.js";
+import config from "./config";
 
-const t = timeago()
-const { covers } = config
+const t = timeago();
+const { covers } = config;
 
 // 是否为移动端
-export const isMobile = /mobile/i.test(window.navigator.userAgent) || document.body.clientWidth < 1200
+export const isMobile =
+  /mobile/i.test(window.navigator.userAgent) ||
+  document.body.clientWidth < 1200;
 
 // 延时
-export const delay = time => new Promise(resolve => setTimeout(resolve, time))
+export const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
 // 文章格式化
 export const formatPost = post => {
-  const { created_at, body, labels } = post
-  const desc = body.split('<!-- more -->')[0]
-  post.desc = desc
-  post.content = body
-  post.cover = covers[(post.number - 1) % covers.length]
-  post.date = t.format(created_at, 'zh_CN')
-  post.filterLabels = labels.sort((a, b) => a.name.length >= b.name.length)
-  return post
-}
+  const { created_at, body, labels } = post;
+  const desc = body.split("<!-- more -->")[0];
+  post.desc = desc;
+  post.content = body;
+  post.cover = covers[(post.number - 1) % covers.length];
+  post.date = t.format(created_at, "zh_CN");
+  post.filterLabels = labels.sort((a, b) => a.name.length >= b.name.length);
+  return post;
+};
 
 // 预加载图片
 export async function loadImgs({ images, width, height }) {
   return new Promise(resolve => {
     const seq = images.map(cover => {
       return new Promise(resolve => {
-        const src = `${cover}?imageView2/2/w/${width}/h/${height}`
-        let imgObj = new Image()
+        const src = `${cover}?imageView2/2/w/${width}/h/${height}`;
+        let imgObj = new Image();
         imgObj.onload = () => {
-          resolve()
-        }
+          resolve();
+        };
         imgObj.onerror = () => {
-          resolve()
-        }
-        imgObj.src = src
+          resolve();
+        };
+        imgObj.src = src;
+      });
+    });
+    Promise.all(seq)
+      .then(() => {
+        resolve();
       })
-    })
-    Promise.all(seq).then(() => {
-      resolve()
-    }).catch(console.error)
-  }).catch(console.error)
+      .catch(console.error);
+  }).catch(console.error);
 }
-
