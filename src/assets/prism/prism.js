f966523f7212de68759839594aa20333d82d34b0
chanshiyu <1124590931@qq.com>
2018-10-14 11:01:04 +0800

    添加java高亮支持

--- src/assets/prism/prism.js
+++ src/assets/prism/prism.js
@@ -17,5 +17,6 @@ import 'prismjs/components/prism-bash'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-css'
 import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-java'
 
 export default Prism
