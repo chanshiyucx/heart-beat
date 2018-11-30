d0ac9259a5d59659b6136dc47b5fa259bfd04127
chenxin <chenxin@funtsui.com>
2018-11-30 15:03:57 +0700

    大小图设置

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,24 +1,24 @@
-import React, { PureComponent } from 'react'
-import router from 'umi/router'
+import React, { PureComponent } from 'react';
+import router from 'umi/router';
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Home from '../pages'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Home from '../pages';
 
-import styles from './index.less'
-import config from '../config'
+import styles from './index.less';
+import config from '../config';
 
-const { backstretch } = config
+const { backstretch } = config;
 
 class App extends PureComponent {
   componentDidMount() {
     // 动态背景
-    window.$('body').backstretch(backstretch.bgImg, backstretch.bgOption)
-    const { pathname } = this.props.location
+    window.$('body').backstretch(backstretch.bgImg, backstretch.bgOption);
+    const { pathname } = this.props.location;
 
     if (pathname !== '/') {
-      router.push(pathname)
-    } 
+      router.push(pathname);
+    }
   }
 
   render({ children }) {
@@ -26,10 +26,10 @@ class App extends PureComponent {
       <div class={styles.container}>
         <Header />
         {children || <Home />}
-        <Footer ref={c => this.footer = c} />
+        <Footer ref={c => (this.footer = c)} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
