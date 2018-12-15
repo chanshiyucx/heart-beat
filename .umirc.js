d0a8a7f243ba27e474988fd54d9114e0a0162145
chenxin <chenxin@funtsui.com>
2018-12-15 19:01:25 +0700

    title

--- .umirc.js
+++ .umirc.js
@@ -2,18 +2,20 @@ export default {
   hash: true,
   history: 'hash',
   plugins: [
-    ['umi-plugin-react', {
-      dva: true,
-      routes: {
-        exclude: [/models\//],
-      },
-      library: 'preact',
-      dynamicImport: {
-        webpackChunkName: true,
-      },
-      pwa: false,
-      fastClick: true,
-      title: '蝉時雨',
-    }],
+    [
+      'umi-plugin-react',
+      {
+        dva: true,
+        routes: {
+          exclude: [/models\//]
+        },
+        library: 'preact',
+        dynamicImport: {
+          webpackChunkName: true
+        },
+        fastClick: true,
+        title: '蝉時雨'
+      }
+    ]
   ]
 }
